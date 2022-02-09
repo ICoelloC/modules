473acab2dba4cecdd671aa1b1357748244413856
Iván Coello Cortés <ivancoellocortes@gmail.com>
2022-02-09 09:13:43 +0100

    Añadidos nuevos campos al modelo

--- school/models/models.py
+++ school/models/models.py
@@ -7,12 +7,11 @@ class student(models.Model):
     _name = 'school.student'
     _description = 'school.student'
 
-    name = fields.Char()
-    # value = fields.Integer()
-    # value2 = fields.Float(compute="_value_pc", store=True)
-    # description = fields.Text()
-
-    # @api.depends('value')
-    # def _value_pc(self):
-    #     for record in self:
-    #         record.value2 = float(record.value) / 100
+    name = fields.Char(string = "Nombre", readonly = False, required = True, help = "Este es el nombre")
+    birth_year = fields.Integer()
+    description = fields.Text()
+    inscription_date = fields.Date()
+    last_login = fields.Datetime()
+    is_student = fields.Boolean()
+    photo = fields.Binary()
+    
