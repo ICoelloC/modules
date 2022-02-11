4adb01098259f4963a04d087df49537159bb661e
Iván Coello Cortés <ivancoellocortes@gmail.com>
2022-02-11 11:56:42 +0100

    añadido el segundo modelo

--- dev_meet/models/models.py
+++ dev_meet/models/models.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class dev_meet(models.Model):
-#     _name = 'dev_meet.dev_meet'
-#     _description = 'dev_meet.dev_meet'
+class developer(models.Model):
+    _name = 'dev_meet.developer'
+    _description = 'Developers'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string='Nombre', required=True,
+                       help='Nombre del desarrollador')
+    nickname = fields.Char(string='Apellidos', required=True,
+                           help='Apellidos del desarrollador')
+    dni = fields.Char(string='DNI', required=True, help='DNI')
+    email = fields.Char(string='Email', help='Correo electrónico')
+    phone = fields.Char(string='Teléfono', help='Teléfono móvil')
+
+
+class technology(models.Model):
+    _name = 'dev_meet.technology'
+    _description = 'Developers'
+
+    name = fields.Char(string='Nombre', required=True,
+                       help='Nombre de la tecnología')
+    logo = fields.Image(string='Logo', help='Logo de la tecnología')
+    official_web = fields.Char(
+        string='Página oficial', help='Página web oficial')
